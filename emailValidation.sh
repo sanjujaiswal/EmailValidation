80c0cf14c25e589ad83ecc0b6212f068b4ac0286
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-11 10:53:09 +0530

    ADDED: Validate 2nd part of email and ensure @

--- emailValidation.sh
+++ emailValidation.sh
@@ -3,13 +3,14 @@ echo "---Email Validation---"
 
 #Constants declaration
 EMAIL_PATTERN1="^[a-z0-9]{2,}$"
+EMAIL_PATTERN2="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]{2,}$"
 
 #Variables declaration
 email=""
 
 #Email Id validation
 read -p "Enter email : " email
-if [[ $email =~ $EMAIL_PATTERN1 ]]
+if [[ $email =~ $EMAIL_PATTERN2 ]]
 then
 	echo "Valid email-Id!!!"
 else
