250c9dc8829d1ad42241c5a564afa9a2619e69b8
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-11 12:07:27 +0530

    ADDED: Validate special character after 3rd mandatory part

--- emailValidation.sh
+++ emailValidation.sh
@@ -5,13 +5,15 @@ echo "---Email Validation---"
 EMAIL_PATTERN1="^[a-z0-9]{2,}$"
 EMAIL_PATTERN2="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]{2,}$"
 EMAIL_PATTERN3="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}$"
+EMAIL_PATTERN="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+([_+-]{1})+\.[a-z]{2,}$"
+EMAIL_PATTERN4"^*[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+*(.*[_+-].*{1}+)+[a-zA-Z0-9]{5,}$"
 
 #Variables declaration
 email=""
 
 #Email Id validation
 read -p "Enter email : " email
-if [[ $email =~ $EMAIL_PATTERN3 ]]
+if [[ $email =~ $EMAIL_PATTERN4 ]]
 then
 	echo "Valid email-Id!!!"
 else
