a7d4f6c44fedf13735f35f649a1bf60a3cdb28cd
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-11 13:00:15 +0530

    ADDED: Country code contain exactly 2 character (TLD)

--- emailValidation.sh
+++ emailValidation.sh
@@ -5,18 +5,15 @@ echo "---Email Validation---"
 EMAIL_PATTERN1="^[a-z0-9]{2,}$"
 EMAIL_PATTERN2="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]{2,}$"
 EMAIL_PATTERN3="^[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}$"
-
 EMAIL_PATTERN4="^[a-z0-9.+_a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+\.[a-z]{2,}$"
-
-#EMAIL_PATTERN4="^*[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+*(.*[_+-].*{1}+)+\.[a-z]{2,}$"
-#EMAIL_PATTERN_FINAL="^*[a-z0-9._a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+*(.*[_+-].*{1}+)+\.[a-z]{2}$"
+EMAIL_PATTERN_FINAL="^[a-z0-9.+-_a-z0-9]{2,}+@{1}[a-z]+\.[a-z]{2,}+\.[a-z]{2}$"
 
 #Variables declaration
 email=""
 
 #Email Id validation
 read -p "Enter email : " email
-if [[ $email =~ $EMAIL_PATTERN4 ]]
+if [[ $email =~ $EMAIL_PATTERN_FINAL ]]
 then
 	echo "Valid email-Id!!!"
 else
