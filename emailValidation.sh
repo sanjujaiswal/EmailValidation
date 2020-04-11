0af7717d07563a9e78fbc00231f28ec26974d51f
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-11 10:35:15 +0530

    ADDED: Validate first part of email

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 echo "---Email Validation---"
 
+#Constants declaration
+EMAIL_PATTERN1="^[a-z0-9]{2,}$"
+
+#Variables declaration
+email=""
+
+#Email Id validation
+read -p "Enter email : " email
+if [[ $email =~ $EMAIL_PATTERN1 ]]
+then
+	echo "Valid email-Id!!!"
+else
+	echo "Invalid email-Id!!!"
+fi
